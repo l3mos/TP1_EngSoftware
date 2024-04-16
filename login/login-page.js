f15a2c896d458a0a9133eadc7719e195c97281f9
camilanicola <camilanicolads@gmail.com>
2024-04-16 13:44:11 -0300

    correcao pagina login

--- login/login-page.js
+++ login/login-page.js
@@ -1,32 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("login-form");
-    const loginErrorMsg = document.getElementById("login-error-msg");
+    //const loginErrorMsg = document.getElementById("login-error-msg");
 
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const username = loginForm.username.value;
-        const password = loginForm.password.value;
-        if (checkCredentials(username, password)) {
-            alert("You have successfully logged in."); // Mensagem de login correto
-            location.reload();
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const username = loginForm.username.value.trim();
+        const password = loginForm.password.value.trim();
+    
+        if ((username === "vendedor" || username === "gerente") && password === "123") {
+            alert("You have successfully logged in.");
+            if (username === "vendedor") {
+                window.location.href = 'https://www.example.com/vendedor_dashboard.html';
+            } else if (username === "gerente") {
+                document.location.href  = 'pag_inicial/gerente/pag_inicial_gerente.html';
+            }
         } else {
-            loginErrorMsg.style.opacity = 1;
+            alert("Invalid username and/or password");
         }
     });
 
-    function checkCredentials(username, password) {
-        // Verifica se há correspondência nos dados registrados
-        const users = getUsers();
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].username === username && users[i].password === password) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function getUsers() {
-        const usersJSON = localStorage.getItem('users');
-        return usersJSON ? JSON.parse(usersJSON) : [];
-    }
 });
