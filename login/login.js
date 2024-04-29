1962f4b26feff920645837cc2d45e7b006a430ca
L3mos <gabriellemos1901@gmail.com>
2024-04-28 23:24:20 -0300

    Fim

--- login/login.js
+++ login/login.js
@@ -2,19 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("login-form");
     const loginErrorMsg = document.getElementById("login-error-msg");
 
+    // Lista de gerentes
+    const gerentes = [
+        { username: "Lemos", password: "123" },
+        { username: "Camila", password: "123" },
+        { username: "Lucas", password: "123" }
+    ];
+
     loginForm.addEventListener("submit", (e) => {
         e.preventDefault();
         const username = loginForm.username.value.trim();
         const password = loginForm.password.value.trim();
-    
-        if ((username === "vendedor" || username === "gerente") && password === "123") {
-            if (username === "vendedor") {
-                window.location.href = '../vendedor/pag_inicial_vendedor.html';
-            } else if (username === "gerente") {
-                window.location.href = '../gerente/pag_inicial_gerente.html';
-            }
-        }
-        else {
+
+        // Verificar se o usuário e a senha correspondem a um gerente na lista
+        const gerente = gerentes.find((gerente) => gerente.username === username && gerente.password === password);
+        if (gerente) {
+            window.location.href = '../gerente/pag_inicial_gerente.html';
+        } else if (username === "vendedor" && password === "123") {
+            window.location.href = '../vendedor/pag_inicial_vendedor.html';
+        } else {
             loginErrorMsg.style.opacity = 1;
         }
     });
